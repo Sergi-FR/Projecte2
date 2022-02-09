8b21dc5feae1f022eddde999c9d97511527ec7a5
SergiPB <94857445+SergiPB@users.noreply.github.com>
2022-02-09 18:24:57 +0100

    Add files via upload

---
+++ Web/index_menu.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GYM</title>
+</head>
+<body>
+    <h1>Paguina prinsipal amb sessio iniciada</h1>
+</body>
+</html>
