126d81068989050144130dd9385ed6c2238509de
SergiPB <94857445+SergiPB@users.noreply.github.com>
2022-02-09 18:50:17 +0100

    Add files via upload

--- Web/index.php
+++ Web/index.php
@@ -1,7 +1,3 @@
-<?php
-
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GYM</title>
+    <link rel="stylesheet" href="css/estils.css">
 </head>
 <body>
     <header>
