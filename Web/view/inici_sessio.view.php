e4936426b3978bb31fcd1403e54141c3cc63ecf4
Sergi-FR <94852377+Sergi-FR@users.noreply.github.com>
2022-02-28 15:29:02 +0100

    Add files via upload

--- Web/view/inici_sessio.view.php
+++ Web/view/inici_sessio.view.php
@@ -40,10 +40,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GYM</title>
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../css/estils.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
 </head>
 <body>
-    <h1>Inici Sessio</h1>
+
+    <div class="menu">
+        <img src="img/perfil.png" alt="Logo">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="view\inici_sessio.view.php">Login</a>
+                    </li>
+                </ul>
+             </nav>
+    </div>
+
 
     <?php if(!empty($message)): ?>
         <p> <?= $message ?> </p>
@@ -56,5 +77,18 @@
         <input type="submit" value="Login">
 
     </form>
+
+    <footer>
+        <section id="contactar">
+            <div class="contenedor">
+                <h2>Contactans</h2>
+                <div class="info">
+                    <p>(+34) 973 53 62 72</p>
+                    <p>info@gmail.com</p>
+                </div>
+            </div>
+        </section>
+    </footer>
+    
 </body>
 </html>
