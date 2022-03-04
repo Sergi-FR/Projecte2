83bbebd83ceaffb35a58b21311d3baa24296fbf3
SergiPB <94857445+SergiPB@users.noreply.github.com>
2022-03-04 21:20:14 +0100

    Add files via upload

--- Web/php/funcions.php
+++ Web/php/funcions.php
@@ -3,7 +3,7 @@
 function con(){
     
     //Connexio a la BD
-    $connexio = new mysqli("localhost", "root", "root", "projecte2");
+    $connexio = new mysqli("localhost", "root", "costa2021", "projecte2");
 
     //Comprovasio de la connexio
     if($connexio->connect_errno){
@@ -35,10 +35,9 @@ function cur_reg(){
 }
 function visualitzarInfoActCol() {
 
-    $sql="SELECT A.ID_Act as Num_Act, S.ID_Sala as Num_Sala, Durada,Hora_i, Hora_Fi, Aforament, Nom as Monitor,Descripció as Sala_Desc
-    from  activitats A,té T,sales S, monitors M, reserva R, col·lectives C
-    where R.ID_Act=A.ID_Act and
-    A.ID_Act=T.ID_Act and  
+    $sql="SELECT A.ID_Act as Num_Act, S.ID_Sala as Num_Sala, Durada,Hora_i, Hora_Fi, Aforament, Nom as Monitor, S.Descripció as Sala_Desc, A.Descipció as Act, Color
+    from  activitats A,té T,sales S, monitors M, col·lectives C
+    where A.ID_Act=T.ID_Act and  
     T.ID_Sala=S.ID_Sala and 
     M.ID_Sala=S.ID_Sala and
     C.ID_Act=A.ID_Act";
@@ -51,10 +50,9 @@ function visualitzarInfoActCol() {
 
 function visualitzarInfoActInv() {
 
-    $sql="SELECT A.ID_Act as Num_Act, S.ID_Sala as Num_Sala, Durada,Hora_i, Hora_Fi, Aforament, Nom as Monitor,Descripció as Sala_Desc
-    from  activitats A,té T,sales S, monitors M, reserva R, individuals I
-    where R.ID_Act=A.ID_Act and
-    A.ID_Act=T.ID_Act and  
+    $sql="SELECT A.ID_Act as Num_Act, S.ID_Sala as Num_Sala, Durada,Hora_i, Hora_Fi, Aforament, Nom as Monitor, S.Descripció as Sala_Desc, A.Descipció as Act, Color
+    from  activitats A,té T,sales S, monitors M, individuals I
+    where A.ID_Act=T.ID_Act and  
     T.ID_Sala=S.ID_Sala and 
     M.ID_Sala=S.ID_Sala and
     I.ID_Act=A.ID_Act";
@@ -65,6 +63,25 @@ function visualitzarInfoActInv() {
 
 }
 
+function act_insc() {
+    if(!empty($_POST['ins'])) {
+        inscriure();
+    } elseif(!empty($_POST['des'])) {
+        desinscriure();
+    }
+}
+
+function inscriure() {
+
+
+
+}
+
+function desinscriure() {
+
+
+
+}
 
 
 ?>
